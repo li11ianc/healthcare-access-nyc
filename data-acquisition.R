bf79a970e37c4bce3091d1a487112a88101703ff
Lillian Clark <lmc97@duke.edu>
2020-04-02 21:49:33 -0700

    read in data tables from US DHS

---
+++ data-acquisition.R
@@ -0,0 +1,9 @@
+hospitals <- read.csv("data/Hospitals.csv")
+
+pharmacies <- read.csv("data/facility.csv")
+
+nursing_homes <- read.csv("data/Nursing_Homes.csv")
+
+trauma_centers <- read.csv("data/Trauma_Levels.csv")
+
+urgent_care <- read.csv("data/Urgent_Care_Facilities.csv")
