de969a2783cfe81dd4c7102cf77253e9de39e4d7
Thea Dowrich <thea.dowrich@duke.edu>
2020-04-03 15:32:18 +0900

    health insurance coverage

--- data-acquisition.R
+++ data-acquisition.R
@@ -7,3 +7,7 @@ nursing_homes <- read.csv("data/Nursing_Homes.csv")
 trauma_centers <- read.csv("data/Trauma_Levels.csv")
 
 urgent_care <- read.csv("data/Urgent_Care_Facilities.csv")
+
+insurance <- read.csv("data/states.csv")
+
+medicare <- read.csv("data/medicare.csv")
