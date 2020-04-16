41d24b4dcf72f2714b1b7b0950cfbca22e4991ef
Thea Dowrich <thea.dowrich@duke.edu>
2020-04-16 23:23:14 +0900

    changed one thing

--- R/data-acquisition.R
+++ R/data-acquisition.R
@@ -4,6 +4,6 @@ nursing <- read.csv("data/nursing_homes.csv")
 urgent_care <- read.csv("data/urgent_care.csv")
 medicare <- read.csv("data/medicare.csv")
 
-all_facilities <- bind_rows(hospitals, pharmacies, nursing, urgent_care, medicare)
+all_facilities <- bind_rows(hospitals, pharmacies, nursing, urgent_care)
 
 write_csv(all_facilities, "data/all_health_facilities.csv")
